da07e842b2322006013d90035243070c8c3a9ae3
Fervil <fervil.darwin@gmail.com>
2019-02-06 14:34:50 +0100

    Step Get Winner + Clock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Game from './Game';
+import Clignote from './Clignote';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Game/>
+        <Clignote/>
       </div>
     );
   }
--- src/Board.jsx
+++ src/Board.jsx
@@ -9,23 +9,53 @@ export default class Board extends Component {
         this.state = {
             grid: Array(9).fill(null),
             player: 'X',
+            result: false,
         };
+    }
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
     }
     handleClick(index) {
         const grid = [...this.state.grid];
+        if(grid[index] !== null){
+            return 0;
+        }
         grid[index] = this.state.player;
         this.setState({ grid });
-        if (this.state.player == 'X'){
-            this.setState({player:'O'})
-        }else{
-            this.setState({player:'X'})
+        if (this.state.player === 'X') {
+            this.setState({player: 'O'})
+        } else {
+            this.setState({player: 'X'})
         }
     }
     renderCases(index) {
         return <Cases etat={this.state.grid[index]} onClick={()=>this.handleClick(index)}/>;
     }
     render() {
-        const status = 'Next player: '+this.state.player;
+            let status;
+        //status = 'Next player: ' + (this.state.player);
+        const winner = this.calculateWinner(this.state.grid);
+        if (winner) {
+            status = 'Winner is: ' + winner;
+        }else{
+            status = 'Next player: ' + (this.state.player);
+        }
         return (<div>
             <div className="status">{status}</div>
             <div className="board-row">
